6645457b11dd4b677a528c0e08ec67837587808e
trinhtan1234 <trinhtan1703@gmail.com>
2023-10-15 21:07:42 +0700

    a

--- lib/login/screen_login.dart
+++ lib/login/screen_login.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:news/components/button.dart';
-import 'package:news/components/text.dart';
-import 'package:news/components/textfield.dart';
 
 class ScreenLogin extends StatelessWidget {
   const ScreenLogin({super.key});
@@ -9,140 +6,201 @@ class ScreenLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.only(left: 10, right: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Center(
-              child: AppText(
-                text: 'TA',
-                style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w500,
+      appBar: AppBar(
+        title: const Text(
+          'LOGIN',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.deepPurple,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              const Divider(),
+              const Padding(padding: EdgeInsets.only(top: 40)),
+              TextButton(
+                onPressed: () {},
+                child: Center(
+                  child: Container(
+                    // margin: const EdgeInsets.all(20),
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: const Row(
+                      children: [
+                        Padding(padding: EdgeInsets.only(left: 20)),
+                        Icon(
+                          Icons.apple,
+                          size: 20,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 50),
+                        ),
+                        Text('Login with Apple'),
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Center(
-              child: AppText(
-                text: 'HiTA TeamModel ID',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.w500,
+              const Padding(padding: EdgeInsets.only(top: 5)),
+              TextButton(
+                onPressed: () {},
+                child: Center(
+                  child: Container(
+                    // margin: const EdgeInsets.all(20),
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: const Row(
+                      children: [
+                        Padding(padding: EdgeInsets.only(left: 20)),
+                        Icon(
+                          Icons.facebook,
+                          size: 20,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 50),
+                        ),
+                        Text('Login with Facebook'),
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const AppTextField(
-              textHint: 'Enter your username',
-              labelTitle: 'Username',
-              errorString: '',
-              obscuetext: false,
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            const AppTextField(
-              textHint: 'Enter your password',
-              obscureText: true,
-              obscuetext: true,
-              errorString: '',
-            ),
-            AppButton(title: 'Login', onPress: () {}),
-            Row(
-              children: [
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      // Navigator.push(context,
-                      //     MaterialPageRoute(builder: (context) {
-                      //   return();
-                      // }));
-                    },
-                    child: const Text('Register'),
+              TextButton(
+                onPressed: () {},
+                child: Center(
+                  child: Container(
+                    // margin: const EdgeInsets.all(20),
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: const Row(
+                      children: [
+                        Padding(padding: EdgeInsets.only(left: 20)),
+                        Icon(
+                          Icons.mail,
+                          size: 20,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 50),
+                        ),
+                        Text('Login with Google'),
+                      ],
+                    ),
                   ),
-                ),
-                const Divider(
-                  color: Colors.blue,
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text('Forget password'),
-                  ),
-                ),
-              ],
-            ),
-            const Center(
-              child: AppText(
-                text: 'More Options',
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.w500,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: AppButton(
-                    title: 'f',
-                    onPress: () {},
+              const Padding(padding: EdgeInsets.only(top: 40)),
+              const Divider(),
+              const Center(
+                child: Text(
+                  'Login with email',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 20)),
+              Container(
+                width: 300,
+                height: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    hintText: 'Email',
+                    prefixIcon: const Icon(Icons.search),
                   ),
                 ),
-                const Divider(
-                  color: Colors.blue,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(right: 10),
-                ),
-                Expanded(
-                  child: AppButton(
-                    title: 'G',
-                    onPress: () {},
-                    color: Colors.grey,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              Container(
+                width: 300,
+                height: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    hintText: 'Password',
+                    prefixIcon: const Icon(Icons.password),
                   ),
                 ),
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 10),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: AppButton(
-                    title: 'Apple',
-                    onPress: () {},
+              ),
+              const Padding(padding: EdgeInsets.only(top: 20)),
+              TextButton(
+                onPressed: () {},
+                child: Center(
+                  child: Container(
+                    // margin: const EdgeInsets.all(20),
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: const Center(
+                        child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    )),
                   ),
                 ),
-                const Divider(
-                  color: Colors.yellow,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(right: 10),
-                ),
-                Expanded(
-                  child: AppButton(
-                    title: 'Chat',
-                    onPress: () {},
-                    color: Colors.green,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Center(
+                  child: Container(
+                    // margin: const EdgeInsets.all(20),
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      // color: Colors.grey,
+                      borderRadius: BorderRadius.circular(12),
+                      // border: Border.all(color: Colors.black),
+                    ),
+                    child: const Center(
+                        child: Text(
+                      'Forgot password',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    )),
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const Padding(padding: EdgeInsets.only(top: 40)),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Do not have an account ?',
+                  ),
+                  TextButton(
+                      onPressed: () {}, child: const Text('Create acount'))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
